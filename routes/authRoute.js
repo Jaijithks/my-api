b706fd91bbdaa7ad1a536abcc72152ce0cb9d03b
jaijithks <kathirjas20@gmail.com>
2026-07-02 15:46:54 +0530

    authentication

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,16 +1,13 @@
 import express from "express";
 import { Router } from "express";
-import { signUP,signIN } from "../controllers/authControllers.js";
+import { signUP, signIN, logOUT } from "../controllers/authControllers.js";
 
 const authRoute = Router();
 
 //routes in auth
 //authRoute.post('/sign-up',signUP);
-authRoute.post('/sign-in',signIN);
-authRoute.get('/log-out',(req,res)=>{
-    res.send('/logout',(req,res)=>{
-        res.send("logout is working")
-    })
-})
+authRoute.post('/sign-in', signIN);
+authRoute.post('/log-out', logOUT);
+authRoute.get('/log-out', logOUT);
 
 export default authRoute;
